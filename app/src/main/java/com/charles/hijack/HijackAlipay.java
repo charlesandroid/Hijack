ad60f729f028a37ce242d20805cf6788bcf33cd6
tiandesheng <tds945@163.com>
2017-05-08 14:41:45 +0800

    no message

--- app/src/main/java/com/charles/hijack/HijackAlipay.java
+++ app/src/main/java/com/charles/hijack/HijackAlipay.java
@@ -14,6 +14,6 @@ public class HijackAlipay extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_alipay);
     }
 }
