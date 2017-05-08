fc2c636882f54b71d8fd58a7d34222a0a92643d5
tiandesheng <tds945@163.com>
2017-05-08 14:49:10 +0800

    no message

--- app/src/main/java/com/charles/hijack/HijackWeiXin.java
+++ app/src/main/java/com/charles/hijack/HijackWeiXin.java
@@ -10,6 +10,6 @@ public class HijackWeiXin extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_weixin);
     }
 }
