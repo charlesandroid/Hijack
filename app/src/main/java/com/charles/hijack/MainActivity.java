b1b71c6e57cd77f6a3b64a629dfb7ae3e1fded0d
tiandesheng <tds945@163.com>
2017-05-08 13:34:15 +0800

    no message

---
+++ app/src/main/java/com/charles/hijack/MainActivity.java
@@ -0,0 +1,13 @@
+package com.charles.hijack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
