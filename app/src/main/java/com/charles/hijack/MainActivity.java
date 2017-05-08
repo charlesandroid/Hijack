8f0be877224128ab7cf3ac46d3db89133c20f1d9
tiandesheng <tds945@163.com>
2017-05-08 14:24:08 +0800

    no message

--- app/src/main/java/com/charles/hijack/MainActivity.java
+++ app/src/main/java/com/charles/hijack/MainActivity.java
@@ -1,13 +1,17 @@
 package com.charles.hijack;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, HijackService.class);
+        startService(intent);
+        Toast.makeText(this, "Hijack start", Toast.LENGTH_SHORT).show();
     }
 }
